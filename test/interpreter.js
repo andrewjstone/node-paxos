d7c5639f2cea983d5f8b24c756d8554e670fd7c6
Andrew Stone <andrew.j.stone.1@gmail.com>
2012-07-24 00:25:48 -0400

    add a simple interpreter - only the set instruction exists so far

---
+++ test/interpreter.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var db = require('../lib/db');
+var interpreter = require('../lib/interpreter');
+
+describe('parse 1 instruction', function() {
+  var instructions = null;
+  it('succeeds and returns a list of functions', function() {
+    instructions = interpreter.parse('s users andrew hashedPass');
+    assert(Array.isArray(instructions));
+  });
+
+  it('running instructions sets the key andrew to hashedPass in the users hash', function() {
+    assert(!db.users);
+    instructions.forEach(function(exec) {
+      exec();
+    });
+    db.users.andrew = 'hashedPass';
+  });
+});
