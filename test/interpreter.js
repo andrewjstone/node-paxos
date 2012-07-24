6365988e55c8e78fb846406664661c2cec148879
Andrew Stone <andrew.j.stone.1@gmail.com>
2012-07-24 00:32:04 -0400

    fix up interpreter test a bit and add header comment to interpreter.parse

--- test/interpreter.js
+++ test/interpreter.js
@@ -7,6 +7,9 @@ describe('parse 1 instruction', function() {
   it('succeeds and returns a list of functions', function() {
     instructions = interpreter.parse('s users andrew hashedPass');
     assert(Array.isArray(instructions));
+    instructions.forEach(function(instruction) {
+      assert.equal(typeof instruction, 'function');
+    });
   });
 
   it('running instructions sets the key andrew to hashedPass in the users hash', function() {
